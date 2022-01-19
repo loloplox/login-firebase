5100877536aa60088c077e220b8b8a90148685e7
loloplox <rodrigomaldonadov8@gmail.com>
2022-01-19 15:06:45 -0500

    Por fin

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { Logueo } from './components/Logueo';
 import { Home } from "./components/Home";
 import firebaseApp from "./credenciales";
